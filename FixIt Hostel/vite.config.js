ffcb5a34a0081913ac92183cdc461781a74fa3d6
Your Name <your-email@gmail.com>
2026-01-31 15:10:11 +0530

    Fix Vercel 404 and routing

--- FixIt Hostel/vite.config.js
+++ FixIt Hostel/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/fixit/",
   server: {
     https: false,
     host: '0.0.0.0',
